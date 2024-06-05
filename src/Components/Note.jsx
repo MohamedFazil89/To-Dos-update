4e6e1c3680abab8f0ebf4044f4cad4ecd10c842f
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-06-05 22:34:05 +0530

    sdf

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import '../styles/Note.css'
 
 
-function Note(props){
-    const [isDone, setIsDone] = useState(false);
-
-    
+function Note(props) {
+    const [isDone, setIsDone] = useState(true);
 
 
-    return(
-        
+
+
+    return (
+
         <div className="Note" onClick={() => props.Delete(props.id)} style={props.styles}>
             <li>{props.list}</li>
         </div>
